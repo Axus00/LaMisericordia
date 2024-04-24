d9ff12974bcf92cac97abe18fbc599179ffc5d95
Fernando <fjgt2000@gmail.com>
2024-04-23 20:21:41 -0500

    Se agrega controller

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMisericordia.Controllers;
+
+public class EmailController : Controller
+{
+    //vistas
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
