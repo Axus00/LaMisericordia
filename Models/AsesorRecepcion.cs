34abea14adbd1cf0bb4c9e520462db523b5618d7
Fernando <fjgt2000@gmail.com>
2024-04-23 19:37:20 -0500

    se crea configuración de modelos y BaseContext

---
+++ Models/AsesorRecepcion.cs
@@ -0,0 +1,8 @@
+﻿namespace LaMisericordia.Models;
+
+public class AsesorRecepcion
+{
+    public int Id { get; set; }
+    public string Correo { get; set; }
+    public string Contrasena { get; set; }
+}
