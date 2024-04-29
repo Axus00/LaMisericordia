b76f7f0e5ce46f2371117164bf7cc8f6eed8bb15
davidmachadoPY <avidmachado@gmail.com>
2024-04-28 20:43:42 -0500

    Cmabios Finales Machado

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,8 +1,12 @@
-﻿namespace LaMisericordia.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace LaMisericordia.Models;
 
 public class Usuario
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingresa tu número de documento.")]
     public string DocumentoIdentidad { get; set; }
+    [Required(ErrorMessage = "Por favor selecciona tu tipo de documento.")]
     public string typeDocument { get; set; }
 }
